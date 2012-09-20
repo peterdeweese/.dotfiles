0b94ee136551577eeb16a5d8ad0974568c35d8fb
Peter DeWeese <peter.deweese@gmail.com>
2012-09-20 13:11:24 -0400

    Added add_command function and gca.

--- functions.sh
+++ functions.sh
@@ -1,3 +1,10 @@
 function add_path {
     checkInPath $1 > /dev/null && echo "$1 is already in PATH." || export PATH=$PATH:$1
 }
+
+function add_command {
+    echo "Adding command $1"
+    echo "$2" > ~/.dotfiles/bin/"$1"
+    chmod +x ~/.dotfiles/bin/"$1"
+}
+
