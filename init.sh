b3a43771370483efe788dffe8e9a135f6b3be37e
Peter DeWeese <peter.deweese@gmail.com>
2013-02-04 14:53:06 -0500

    Removed trailing slash from path.

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ PATH=/usr/local/bin:$PATH
 . ~/.dotfiles/aliases.sh
 . ~/.dotfiles/functions.sh
 
-add_path ~/.dotfiles/bin/
+add_path ~/.dotfiles/bin
 
 echo "https://github.com/peterdeweese/.dotfiles loaded."
